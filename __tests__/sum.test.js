4638acd62f15787771dc8c3dd63c14c48957871e
Monaf Daod <monaf2daod@gmail.com>
2022-07-26 00:01:51 +0300

    writing Description for test blocks

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,17 +1,23 @@
 const Sum = require("../sum");
 
-test("Testing the sum of no number", () => {
-  expect(Sum()).toBe(0);
+describe("Testing Block Num 1 : No number", () => {
+  test("Testing the sum of no number", () => {
+    expect(Sum()).toBe(0);
+  });
 });
 
-test("Testing the sum of one number", () => {
-  expect(Sum(1)).toBe(1);
+describe("Texting Block Num 2 : Only one number", () => {
+  test("Testing the sum of one number", () => {
+    expect(Sum(1)).toBe(1);
+  });
 });
 
-test("Testing the sum of two numbers", () => {
-  expect(Sum(1, 2)).toBe(3);
-});
+describe("Testing Block Num 3 : More than one number", () => {
+  test("Testing the sum of two numbers", () => {
+    expect(Sum(100, 150)).toBe(250);
+  });
 
-test("Testing the sum of three numbers", () => {
-  expect(Sum(1, 2, 3)).toBe(6);
+  test("Testing the sum of three numbers", () => {
+    expect(Sum(100, 200, 300)).toBe(600);
+  });
 });
